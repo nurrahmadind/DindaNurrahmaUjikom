23f254e49d59cbbaf6607deb3018bc7e2ddfdd85
nurrahmadind <dinda.smkn6@gmail.com>
2025-11-20 10:16:09 +0700

    Karakter Sctipt
    Mencoba menggeser karakter

---
+++ Assets/Scripts/Karakter.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Karakter : MonoBehaviour
+{
+   public Transform CubeTransform;
+   public Vector2 PosisiKlik;
+
+   void Start()
+    {
+    
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Update()
+    {
+        //geser objek ke mouse
+        Vector2 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // CubeTransform.position = worldMousePosition;
+        // CubeTransform.position = Vector2.MoveTowards (CubeTransform.position, worldMousePosition,0.05f);
+        //geser object ke klik
+        Vector2 minimum = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        //untuk menentukan posisi minimum stelah diubah menjadi posisi world
+        Vector2 maximum = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,0, 0));
+        //untuk menentukan posisi maksimum stelah diubah menjadi posisi world
+
+        worldMousePosition.x = Mathf.Clamp(worldMousePosition.x, minimum.x, maximum.x);
+        //untuk menentukan batas x dari posisi mouse
+        worldMousePosition.y = -4.5f;
+
+        CubeTransform.position = worldMousePosition;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+           //ini harusnya nanti spawn object
+        }
+        // CubeTransform.position = Vector2.MoveTowards (CubeTransform.position, PosisiKlik,0.1f);
+        //fungsi ini berguna untuk menggeser objek ke posisi yang telah ditentukan
+        //0.1f adalah kecepatan gerak
+        //cubetransform.position adalah posisi objek yang akan digeser
+        //posisiklik adalah posisi yang dituju
+
+        // if (Input.GetKeyDown(KeyCode.Mouse0))
+        // {
+        //     SpawnSegitiga();
+        // }
+        
+    }
+
+}
