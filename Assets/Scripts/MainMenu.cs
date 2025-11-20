f8bed3abe877664c25c4c0c24cfd0e5db43e02e5
nurrahmadind <dinda.smkn6@gmail.com>
2025-11-20 09:42:20 +0700

    Membuat Script MainMenu
    Membuat script untuk berpindah ke halaman credit dan exit game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,31 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public TMP_Text text;
+    
+     public void GamePlay()
     {
-        
+        SceneManager.LoadScene("GamePlay");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Credit()
     {
+        SceneManager.LoadScene("Credit");
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Keluar dari Game");
         
+        // Perintah standar untuk keluar dari aplikasi. 
+        // Ini hanya akan bekerja saat game sudah di-build (file .exe).
+        Application.Quit();
+
+        // Optional: Baris di bawah ini hanya untuk membuat tombol Quit berfungsi
+        // saat Anda mengujinya di Unity Editor. Hapus baris ini saat di-build.
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
