92f92194a5ea0a98f06cb68528a58f8cb7477f6f
nurrahmadind <dinda.smkn6@gmail.com>
2025-11-20 13:20:54 +0700

    score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,76 +1,24 @@
-// using TMPro;
-// using UnityEngine;
-// using UnityEngine.SceneManagement;
-
-// public class Score : MonoBehaviour
-// {
-//     public TMP_Text scoreText;
-//     // public AudioObject audioObject;
-//     public int score;
-//     public int targetScore = 100;
-//     public int highScore;
-//     // Start is called once before the first execution of Update after the MonoBehaviour is created
-//     void Start()
-//     {
-//         score = 0;
-     
-
-//     public void tambahScore()
-//     {
-//         score += 5;
-//     }
-
-//     public void kurangScore()
-//     {
-
-//     }
+using TMPro;
+using UnityEngine;
 
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         // if(Input.GetKeyDown(KeyCode.Mouse0))
-//         // {
-//         //     Vector2 posisiKlik = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+public class Score : MonoBehaviour
+{
+    public TMP_Text scoreText;
+    // public AudioObject audioObject;
+    public int score;
+        void Start()
+    {
+      UpdateScore();
+    }
 
-//         //     Collider2D objectTerdeteksi = Physics2D.OverlapPoint(posisiKlik);
+    public void UpdateScore()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
 
-//         //     // GameObject objectHapus = objectTerdeteksi.gameObject;
-
-//         //     if (objectTerdeteksi != null)
-//         //     {
-//         //         if(objectTerdeteksi.CompareTag("daurulang"))
-//         //         {
-//         //             GameObject objectHapus = objectTerdeteksi.gameObject;
-//         //             Debug.Log("sampah daur ulang diklik");
-//         //             // Debug.Log("Object dihapus: " + objectHapus.name);
-//         //             Destroy(objectHapus);
-//         //             tambahScore();
-//         //             audioObject.putarSFXDaurUlang();
-//         //         }
-//         //         else if(objectTerdeteksi.CompareTag("tdkdaurulang"))
-//         //         {
-//         //             GameObject objectHapus = objectTerdeteksi.gameObject;
-//         //             Debug.Log("sampah tidak daur ulang diklik");
-//         //             // Debug.Log("Object dihapus: " + objectHapus.name);
-//         //             Destroy(objectHapus);
-//         //             kurangScore();
-//         //             audioObject.putarSFXTdkDaurUlang();
-//         //         }
-//         //         else
-//         //         {
-//         //             GameObject objectHapus = objectTerdeteksi.gameObject;
-//         //             Debug.Log("racun diklik");
-//         //             // Debug.Log("Object dihapus: " + objectHapus.name);
-//         //             Destroy(objectHapus);
-//         //             //score = 0;
-//         //             audioObject.putarSFXRacun();
-//         //             score = 0;
-//         //             PlayerPrefs.SetInt("lastscore", score);
-//         //             SceneManager.LoadScene("GameOver");
-//         //         }
-//         //         scoreText.text = "Score: " + score.ToString();
-//         //     }
-//         }
-//     }
-// }
+    public void tambahScore()
+    {
+       score++;
+    }
+}
