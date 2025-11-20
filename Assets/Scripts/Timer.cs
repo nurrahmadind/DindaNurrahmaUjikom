14a3b8d1320bbff1c88ed5de7007f8abb473188d
nurrahmadind <dinda.smkn6@gmail.com>
2025-11-20 11:43:43 +0700

    mencoba membuat fill timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Timer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float waktu; //float ialah class untuk menampung angka desimal
+    //waktu adalah variable yang menampung nilai dari waktu
+    
+    public int MaximumWaktu;
+    //int ialah class untuk menampung angka bulat
+    //MaximumWaktu adalah variable yang menampung nilai dari waktu maksimal
+      public bool WaktuBerjalan;
+    //bool berguna untuk menampung nilai true atau false
+    //WaktuBerjalan adalah variable yang menampung nilai dari waktu berjalan
+    //bool digunakan untuk menentukan apakah waktu berjalan atau tidak
+
+    // public KeyCode StartCoroutineKey;
+    // //Keycode ialah class untuk menampung keycode
+    // //StartCoroutineKey adalah variable yang menampung nilai dari keycode untuk memulai coroutine
+
+    // public KeyCode StopCoroutineKey;
+    // //StopCoroutineKey adalah variable yang menampung nilai dari keycode untuk menghentikan coroutine
+
+    // public Coroutine HitungTimerCoroutine;
+    // //Coroutine ialah class untuk menampung coroutine
+    // //coroutine berfungsi untuk menjalankan perintah secara berurutan
+    // //HitungTimerCoroutine adalah variable yang menampung nilai dari coroutine
+
+    public Image ProgressFill;
+
+    // Update is called once per frame
+    void Start()
+    {
+        StartCoroutine(HitungTimer());
+        waktu=10;        
+    }
+
+   void Update()
+    {
+        if (waktu <= 0)
+        
+            {
+                SceneManager.LoadScene("GameOver");
+            
+            }
+    }
+    IEnumerator HitungTimer()
+    {
+        while (WaktuBerjalan == true)// fungsi pada baris ini berfungsi untuk menampilkan timer
+        //waktu berjalan adalah boolean yang menentukan apakah timer berjalan atau tidak
+        //waktu < MaximumWaktu adalah untuk menentukan apakah waktu sudah mencapai batas maksimal atau belum
+        //waktu adalah variabel yang menentukan berapa detik yang sudah berjalan
+        //MaximumWaktu adalah variabel yang menentukan berapa detik yang akan berjalan
+        {
+        waktu = waktu -1; //waktu akan bertambah setiap detik
+        ProgressFill.fillAmount = waktu / 10;
+        
+        yield return new WaitForSeconds(1);
+        }
+    }
+}
