bb96436ab3c83d3e023d9ff88050d7b44f707652
nurrahmadind <dinda.smkn6@gmail.com>
2025-11-20 10:46:03 +0700

    Audio
    memasukan audio untuk masing-masing scene

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //Audio Pemain Menembak
+    // Musuh sampai bawah layar
+    // Peluru tabrak musuh
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
